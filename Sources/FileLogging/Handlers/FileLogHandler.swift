567dcd7b14201d544e4e8cfce44f99ffb1513f58
Jacob Williams <ponyboy47@gmail.com>
2019-07-05 10:46:35 -0600

    Use the ConstantStreamFileHandler protocol

--- Sources/FileLogging/Handlers/FileLogHandler.swift
+++ Sources/FileLogging/Handlers/FileLogHandler.swift
@@ -1,19 +1,15 @@
 import Logging
 import TrailBlazer
 
-public struct FileLogHandler: FileHandler {
-    // swiftlint:disable identifier_name
-
+public struct FileLogHandler: ConstantStreamFileHandler {
     /// An opened file that can be written to
-    public var _stream: FileStream!
-
-    // swiftlint:enable identifier_name
+    public let stream: FileStream
 
     /// The encoding to use when converting a String log message to bytes which can be written to the file
     public var encoding: String.Encoding
 
     /// The label for the handler
-    public var label: String
+    public let label: String
     /// The minimum level allowed for levels to be written to file
     public var logLevel = Logger.Level.info
     /// Special Logger metadata
@@ -21,7 +17,7 @@ public struct FileLogHandler: FileHandler {
 
     public init(label: String, opened file: FileStream, encoding: String.Encoding) {
         self.label = label
-        _stream = file
+        stream = file
         self.encoding = encoding
     }
 }
